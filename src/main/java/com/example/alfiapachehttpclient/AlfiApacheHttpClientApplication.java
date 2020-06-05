4a50e155e4c23b7bc46e0c5e6e1b38436f36b4d9
Mark Franklin <mark@gremlin.com>
2020-06-05 11:59:29 -0700

    initial commit

---
+++ src/main/java/com/example/alfiapachehttpclient/AlfiApacheHttpClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.alfiapachehttpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfiApacheHttpClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfiApacheHttpClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/alfiapachehttpclient/AlfiApacheHttpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alfiapachehttpclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfiApacheHttpClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
